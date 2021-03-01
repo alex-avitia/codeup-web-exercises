2e79b26f4f6d33ca64953d3a208782ee5b3b57e8
Alex Avitia <alex.avitia77@gmail.com>
2021-03-01 16:56:34 -0600

    feat: started on circles.js exercise

---
+++ js/circle.js
@@ -0,0 +1,39 @@
+(function() {
+    "use strict";
+
+    // create a circle object
+    var circle = {
+        radius: 3,
+
+        getArea: function (radius) {
+
+            return Math.PI * Math.pow(radius, 2);
+        },
+
+        logInfo: function (doRounding) {
+            // TODO: complete this method.
+            if (doRounding === true) {
+                return Math.round();
+            }
+            // If doRounding is true, round the result to the nearest integer.
+            // Otherwise, output the complete value
+
+            console.log("Area of a circle with radius: " + this.radius + ", is: ");
+        }
+    };
+
+    // log info about the circle
+    console.log("Raw circle information");
+    circle.logInfo(false);
+    console.log("Circle information rounded to the nearest whole number");
+    circle.logInfo(true);
+
+    console.log("=======================================================");
+    // TODO: Change the radius of the circle to 5.
+
+    // log info about the circle
+    console.log("Raw circle information");
+    circle.logInfo(false);
+    console.log("Circle information rounded to the nearest whole number");
+    circle.logInfo(true);
+})();
