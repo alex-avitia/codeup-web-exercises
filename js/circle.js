6c99d631ffb0bd70ec4c9543b413da849c288ab7
Alex Avitia <alex.avitia77@gmail.com>
2021-03-02 09:41:52 -0600

    feat: removed unnecessary TODOs

--- js/circle.js
+++ js/circle.js
@@ -6,15 +6,11 @@
         radius: 3,
 
         getArea: function () {
-            // TODO: complete this method
              return Math.PI * Math.pow(this.radius, 2);
-            // hint: area = pi * radius^2
-
-             // TODO: return the proper value
         },
 
         logInfo: function (doRounding) {
-            // TODO: complete this method.
+
             if (doRounding === true) {
              console.log("Area of a circle with radius: " + this.radius + ", is: " + Math.round(this.getArea()));
             } else {
@@ -30,7 +26,7 @@
     circle.logInfo(true);
 
     console.log("=======================================================");
-    // TODO: Change the radius of the circle to 5.
+    // change radius to 5
     circle.radius = 5;
     // log info about the circle
     console.log("Raw circle information");
