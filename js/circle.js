c002e4649caa6e181252274ca3253f6c6f766de1
Alex Avitia <alex.avitia77@gmail.com>
2021-03-02 09:40:47 -0600

    feat: add proper output to circle.js with radius of 3 and 5

--- js/circle.js
+++ js/circle.js
@@ -1,24 +1,25 @@
-(function() {
+(function (x) {
     "use strict";
 
     // create a circle object
     var circle = {
         radius: 3,
 
-        getArea: function (radius) {
+        getArea: function () {
+            // TODO: complete this method
+             return Math.PI * Math.pow(this.radius, 2);
+            // hint: area = pi * radius^2
 
-            return Math.PI * Math.pow(radius, 2);
+             // TODO: return the proper value
         },
 
         logInfo: function (doRounding) {
             // TODO: complete this method.
             if (doRounding === true) {
-                return Math.round();
+             console.log("Area of a circle with radius: " + this.radius + ", is: " + Math.round(this.getArea()));
+            } else {
+                console.log("Area of a circle with radius: " + this.radius + ", is: " + this.getArea());
             }
-            // If doRounding is true, round the result to the nearest integer.
-            // Otherwise, output the complete value
-
-            console.log("Area of a circle with radius: " + this.radius + ", is: ");
         }
     };
 
@@ -30,7 +31,7 @@
 
     console.log("=======================================================");
     // TODO: Change the radius of the circle to 5.
-
+    circle.radius = 5;
     // log info about the circle
     console.log("Raw circle information");
     circle.logInfo(false);
