470ea8b99446def6c0108ffb353e14b5060a3889
Alex Avitia <alex.avitia77@gmail.com>
2021-04-06 12:27:35 -0500

    finished: jquery selectors exercise

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -1,3 +1,26 @@
+/* removed code from previous exercises
 $(document).ready(function() {
     alert( 'The DOM has finished loading!' );
 });
+*/
+
+/* Pt I Selectors
+
+var headsUp = $('#main-header').html();
+alert(headsUp);
+
+ */
+
+/* Pt II Selectors
+
+$('.codeup').css('border', '1px solid red');
+
+ */
+
+/* Pt III Selectors
+
+$('li').css('font-size', '20px');
+
+$('h1, p, li').css('background-color', 'yellow');
+
+ */
