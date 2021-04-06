89399a727c737c10471e58d0b644e2d82014eccb
Alex Avitia <alex.avitia77@gmail.com>
2021-04-06 13:42:47 -0500

    finished: JQuery mouse exercise

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -24,3 +24,23 @@ $('li').css('font-size', '20px');
 $('h1, p, li').css('background-color', 'yellow');
 
  */
+
+/* JQuery Mouse Events
+
+ */
+
+$('h1').click(function() {
+    $('h1').css('background-color', 'cornflowerblue');
+});
+
+$('p').dblclick(function(){
+    $('p').css('font-size', '18px');
+});
+
+$('li').hover(
+    function() {
+        $(this).css('color', 'red');
+    },
+    function() {
+        $(this).css('color', 'black');
+    })
