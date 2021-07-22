8fc7c129853706129c501ebe66054ec2dfae5743
Alex Avitia <alex.avitia77@gmail.com>
2021-07-22 18:47:17 -0500

    updated weather map project

--- js/weather_map.js
+++ js/weather_map.js
@@ -1,69 +1,151 @@
- "use strict";
+"use strict";
 
- const searchForm = document.getElementById("searchForm");
- const searchInput = document.getElementById("searchInput");
- const searchBtn = document.getElementById("searchBtn");
+$(document).ready(function(){
+    let marker = new mapboxgl.Marker({
+        color: "#568a7f",
+        draggable: true
+    });
 
- searchForm.addEventListener("submit", (e) => {
-     e.preventDefault();
- });
+    function error(err){
+        console.log(err.code, err.message);
+    }
 
- searchBtn.addEventListener("click", async () => {
-     if (searchInput.value === "") return;
-     const weatherData = await weather.getData(searchInput.value);
-     view.setSearchResult(weatherData);
- });
+    let options = {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0
+    }
+    navigator.geolocation.getCurrentPosition(function (pos){
+        let userCords = pos.coords;
+        let latLng = [userCords.longitude, userCords.latitude,];
+        marker.setLngLat(latLng).addTo(map);
+        map.flyTo({ center: latLng, zoom: 11 });
+        marker.on("dragend", () => forecast([marker.getLngLat().lat, marker.getLngLat().lng]));
+        forecast([userCords.latitude, userCords.longitude]);
+    }, error, options);
 
-//mapbox token
- mapboxgl.accessToken = mapboxToken;
+//map
+    mapboxgl.accessToken = mapboxToken;
+    let map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center: [-98.2625, 29.8752], // starting position [lng, lat]
+        zoom: 9
+    });
 
- var mapOptions = {
-     container: 'map', // container ID
-     style: 'mapbox://styles/mapbox/dark-v10', // style URL
-     center: [-98.4861, 29.5252], // starting position [lng, lat]
-     zoom: 10
- }
+//search listener
+    $("button").click(function (event) {
+        event.preventDefault();
+        let cityVar = $(".form-control").val();
+        let cityString = cityVar.toString();
+        console.log(cityVar);
+        geocode(cityString, mapboxToken).then(function (data) {
+            console.log(data);
+            let latLng = {
+                lat: data[1],
+                lng: data[0]
+            };
+            marker.setLngLat([data[0],data[1]]);
+            let cityArray = data.reverse();
+            map.flyTo({ center: latLng, zoom: 11 });
+            console.log(cityArray);
+            console.log(forecast(cityArray));
 
- var map = new mapboxgl.Map(mapOptions);
+            marker.on("dragend", () => forecast([marker.getLngLat().lat, marker.getLngLat().lng]));
+        });
+    });
 
- var marcoMap = new mapboxgl.Map({
-    container: "map",
-    style: "mapbox://styles/mapbox/dark-v10",
-    center: [-98.4861, 29.526],
-    zoom: 8
+// map option listener
+
+    $('.form-check-input').change(function (){
+        if (this.checked) {
+            $('.map').removeClass('map-hidden');
+        } else {
+            $('.map').addClass('map-hidden');
+        }
+    })
+//day option listener
+
+    function markerLocation() {
+        let searchCords = marker.getLngLat();
+        forecast([searchCords.lat, searchCords.lng])
+    };
+    $("#day-select").change(function(){
+        $(this).click(markerLocation());
+    });
+
+// weather
+
+    function forecast(array) {
+        reverseGeocode({lat: array[0], lng: array[1]}, mapboxToken).then(function (data){
+            let locationArray = data.split(",")
+            let locState = locationArray[2].split(" ");
+            locState.length = 2;
+            let locationHTML = `<h3>${locationArray[1]},${locState[1]}</h3>`
+            $('.location-box').empty();
+            $('.location-box').append(locationHTML);
+
+        })
+        $.get("https://api.openweathermap.org/data/2.5/onecall", {
+            lat: array[0],
+            lon: array[1],
+            appId: OPEN_WEATHER_APPID,
+            units: "imperial",
+            exclude: "minutely,hourly,alerts"
+        }).done(function (results) {
+            let currentWeather = results.current.weather[0].main;
+            let currentClass = $('body').attr("class");
+            switch (currentWeather) {
+                case "Clouds":
+                    $('body').removeClass(currentClass);
+                    $('body').addClass('overcast');
+                    break;
+                case "Thunderstorm":
+                    $('body').removeClass(currentClass);
+                    $('body').addClass('thunderstorm');
+                    break;
+                case "Drizzle":
+                    $('body').removeClass(currentClass);
+                    $('body').addClass('overcast');
+                    break;
+                case "Snow":
+                    $('body').removeClass(currentClass);
+                    $('body').addClass('snowy');
+                    break;
+                case "Clear":
+                    $('body').removeClass(currentClass);
+                    $('body').addClass('sunny');
+                    break;
+                case "Rain":
+                    $('body').removeClass(currentClass);
+                    $('body').addClass('thunderstorm')
+            }
+            $('.weather-cards').empty();
+            let dayInteval = $('#day-select').val();
+            for(let dayIndex = 0; dayIndex <= dayInteval; dayIndex++) {
+                let forecastData = results.daily[dayIndex];
+                let html =
+                    `<div class="card card-weather">
+                    <div class="card-body">
+                        <div class="weather-date-location">
+                           <p class="text-gray"> <span class="weather-date">${new Date(forecastData.dt * 1000).toDateString()}</span> </p>
+                        </div>
+                        <div class="weather-data d-flex">
+                            <div class="mr-auto">
+                                <p class="display-3">Low ${forecastData.temp.min}°F High ${forecastData.temp.max}°F</p>
+                                <img src="http://openweathermap.org/img/wn/${forecastData.weather[0].icon}@2x.png">
+                                <p>${forecastData.weather[0].description}</p>
+                                <p>Humidity: ${forecastData.humidity}</p>
+                                <p>Wind speed: ${forecastData.wind_speed}</p>
+                                <p>Pressure: ${forecastData.pressure}</p>
+                            </div>
+                        </div>
+                    </div>
+                        </div>
+                    </div>
+                </div>`
+                $('.weather-cards').append(html);
+            }
+        });
+    }
 })
-
-    //console logs weather in SA
- $.get("https://api.openweathermap.org/data/2.5/onecall", {
-    appid: OPEN_WEATHER_APPID,
-    lat: 29.423017,
-    lon: -98.48527,
-    units: "imperial",
-    exclude: "current,minutely,hourly,alerts"
-}).done(function(results){
-    console.log(results);
-})
-
-    //dis my marker
- var currentLocationMarker = new mapboxgl.Marker({color: "#ffc107", draggable: true})
-    .setLngLat([-98.4861, 29.426])
-    .addTo(marcoMap);
-
- currentLocationMarker.on("dragend", () => markerDragEnd());
-    //displays coords of marker
-    function markerDragEnd() {
-    var markerCoords = currentLocationMarker.getLngLat();
-    console.log(markerCoords)
-}
-
-    // -Update your layout and AJAX request(s) to display a five day forecast for the city you live in that looks like the screenshot below.
-    //
-    // -If you want to add the icons the URLs for OpenWeatherMap's icons are formatted like: http://openweathermap.org/img/w/[icon].png where [icon] comes from the API response.
-    //
-    // -Refer to your Mapbox API exercise. Recreate the map below your 5 day forecast. Read through the documentation for the Mapbox API and figure out how to allow the user to drop a pin on any place on the map. Once the user drops a pin, grab its coordinates and feed those into your OpenWeatherMap API. Update the five-day forecast with the information from those coordinates (you should also get rid of your input boxes at this point).
-    //
-    // -Add a Mapbox text input to search by location and have the forecast update when a new location is searched.
-    //
-    // -As a bonus make sure you can update the marker's position to the new search result.
-
-
